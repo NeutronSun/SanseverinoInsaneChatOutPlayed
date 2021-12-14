e17b5fda7d15375956f7fb482a4b85cf6a4f6aa5
NeutronSun <lorenzosanseverino2003@gmail.com>
2021-12-14 17:16:48 +0100

    Update FileManager.java

--- ServerFiles/Server/src/FileManager.java
+++ ServerFiles/Server/src/FileManager.java
@@ -18,7 +18,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
-import java.util.Random;
 import java.util.Base64.Encoder;
 
 /**
